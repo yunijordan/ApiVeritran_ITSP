abd733e14156c2cdc051842dd2957652e5b43d06
Roberto Sarmiento <roberto@ebizmarts.com>
2020-04-19 17:20:58 -0300

    Adding cardArtData

---
+++ ApiV/src/main/java/com/veritran/api/rest/common/cardArtData.java
@@ -0,0 +1,19 @@
+package com.veritran.api.rest.common;
+
+public class cardArtData {
+	private String[] cardArtRefID;
+
+	public cardArtData(String[] cardArtRefID) {
+		super();
+		this.cardArtRefID = cardArtRefID;
+	}
+
+	public String[] getCardArtRefID() {
+		return cardArtRefID;
+	}
+
+	public void setCardArtRefID(String[] cardArtRefID) {
+		this.cardArtRefID = cardArtRefID;
+	}
+
+}
